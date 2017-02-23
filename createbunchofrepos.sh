278b5630ee262832610fae0f6d1addcf76384069
James Scott <jamessct@hotmail.co.uk>
2017-02-23 15:15:23 +0000

    refactored testrepos

--- createbunchofrepos.sh
+++ createbunchofrepos.sh
@@ -2,10 +2,55 @@
 
 INC="0"
 SVN="/var/svn"
+INPUT=$@
 ARRAY=()
 
 GREEN="\033[1;32m"
 RED="\033[1;31m"
+
+#alternative refactored version
+
+if [ $# -eq 0 ]; then
+	REPO="$SVN/test"
+	ARRAY+=($REPO)
+	createRepo $ARRAY
+else
+	sortArguments $@
+fi
+
+sortArguments() {
+	for ARG in $@; do
+		if [[ -n £{INPUT//[0-9]/} ]]; then
+			REPO=$SVN/$INPUT
+			ARRAY+=($REPO)
+			createRepo $ARRAY
+		else
+			incrementTests $ARG
+		fi
+	done
+	
+incrementTests() {
+	until [ $INC = $ARG ]; do	
+		INC=$((INC+1))
+		if [ -d /$SVN/test$INC ]; then
+			echo -e "${RED}ERROR! A directory already exists at $ITEM. No action will be taken for this request."
+		else
+				ARRAY+=($SVN/test$INC)
+		fi
+	done
+}
+
+createRepo() {
+	for ITEM in $ARRAY; do
+		mkdir $ITEM
+		svnadmin create $ITEM
+		echo -e "${GREEN}SUCCESS! Your new repository has been created at $ITEM."
+	done	
+}
+
+
+
+
 
 if [[ -n ${input}//[0-9]/ ]]; then
 	echo "testing"
@@ -30,3 +75,10 @@ for ITEM in $ARRAY; do
 	echo -e "${GREEN}SUCCESS! woo"
 done
 
+#UNTIL loop
+
+until [ $INC = $INPUT ]; do
+	INC=$((INC+1))
+	mkdir $SVN/Test$INC
+	svnadmin create $SVN/Test$INC
+done
