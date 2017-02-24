a680c9efbef1a82bd479dfba8d186df56689c0b3
James Scott <jamessct@hotmail.co.uk>
2017-02-24 16:15:25 +0000

    refactored the testrepos script, working fine now I think

--- createbunchofrepos.sh
+++ createbunchofrepos.sh
@@ -3,51 +3,47 @@
 INC="0"
 SVN="/var/svn"
 INPUT=$@
-ARRAY=()
 
 GREEN="\033[1;32m"
 RED="\033[1;31m"
 
 sortArguments() {
-	for ARG in $INPUT; do
-		if [[ -n ${INPUT//[0-9]/} ]]; then
-			if [ -d $SVN/$INPUT ]; then
-				echo -e "${RED}ERROR! A directory already exists at $SVN/$INPUT. No action will be taken for this request."
-			else
-				REPO=$SVN/$INPUT
-				ARRAY+=($REPO)
-			fi
-		else
+	if [[ -n ${INPUT//[0-9]/} ]]; then
+		for ARG in $INPUT; do
+			REPO=$SVN/$INPUT
+			ARRAY+=($REPO)
+		done	
+		createRepo $ARRAY	
+	else	
+		for ARG in $INPUT; do REPO=$SVN/$ARG
 			incrementTests $ARG
-		fi
-	done
-	createRepo $ARRAY[@]
+		done	
+	fi
 }
 	
 incrementTests() {
 	until [ $INC = $ARG ]; do	
 		INC=$((INC+1))
-		if [ -d $SVN/test$INC ]; then
-			echo -e "${RED}ERROR! A directory already exists at $SVN/test$INC. No action will be taken for this request."
-		else
-				ARRAY+=($SVN/test$INC)
-		fi
+		ARRAY+=($SVN/test/$INC)
 	done
 	createRepo $ARRAY[@]
 }
 
 createRepo() {
 	for ITEM in ${ARRAY[@]}; do
-		mkdir $ITEM 2>/Dev/Null
-		svnadmin create $ITEM
-		echo -e "${GREEN}SUCCESS! Your new repository has been created at $ITEM."
+		if [ -d $ITEM ]; then
+			echo -e "${RED}ERROR! A directory already exists at $SVN/$INPUT. No action will be taken for this request."
+		else
+			mkdir $ITEM 2>/Dev/Null
+			svnadmin create $ITEM
+			echo -e "${GREEN}SUCCESS! Your new repository has been created at $ITEM."
 	done	
 }
 
 if [ $# -eq 0 ]; then
 	REPO="$SVN/test"
 	ARRAY+=($REPO)
-	createRepo $ARRAY[@]
+	createRepo $ARRAY
 else
 	sortArguments
 fi
