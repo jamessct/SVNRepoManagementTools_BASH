1aa83f5c6de69674227c29aa2acc7671f73ad353
James Scott <jamessct@hotmail.co.uk>
2017-02-27 09:21:14 +0000

    added changes to backup script

--- backup.sh
+++ backup.sh
@@ -12,23 +12,24 @@ GREEN="\033[1;32m"
 checkForSlashes() {
 	for ARG in ${ARRAY1[@]}; do
 		if [[ $ARG == *"/"* ]]; then
-			for REPO in $ARRAY; do
-				ARRAY2+=($REPO)
-				backupRepos $ARRAY
+			for REPO in $ARRAY1; do
+				ARRAY+=($REPO)
 			done
 		else
-			for REPO in $ARRAY; do
+			for REPO in $ARRAY1; do
 				ARRAY+=($SVNREPO/$REPO)
-				backupRepo $ARRAY
 			done
 		fi
+		backupRepo $ARRAY
 	done
 }
 
 backupRepos() {
 	for REPO in ${ARRAY[@]}; do
 		if [ -f $SVNREPO/$REPO/format ]; then
-			svnadmin dump $SVNREPO/$REPO -r HEAD &>/dev/null | gzip > $TEMP/$REPO.svn.gzip
+			svnadmin dump $SVNREPO/$REPO -r HEAD &>/dev/null | gzip > $TEMP$REPO.svn.gzip
+			cp $TEMP$REPO.svn.gzip $BACKUP$REPO.svn.gzip
+			rm $REMP$REPO.svn.gzip
 			echo -e "${GREEN}SUCCESS! Backup of $REPO complete."
 		else
 			echo -e "${RED}ERROR! The repository $REPO does not exist. No backup has been made for this argument."
@@ -40,7 +41,7 @@ cd $SVNREPO
 
 if [ $# -eq 0 ]; then
 	for REPO in *; do
-		ARRAY+=($REPO)
+		ARRAY+=($SVNREPO/$REPO)
 	done
 	backupRepos $ARRAY
 else
